f7400280305c64ed1b64fc76ba72e0d3e8498a05
LSB0201 <olyme111@naver.com>
2026-06-02 03:34:18 +0000

    led, buzzer 기능 함수 구현

---
+++ src/buzzer.c
@@ -0,0 +1,20 @@
+#include <wiringPi.h>
+
+#include "hardware.h"
+
+#define BUZZER_PIN 6 // WiringPi 6번 (BCM 25)
+
+void set_buzzer(int state) {
+    static int init_flag = 0;
+
+    // init_flag를 사용해 최초 한 번만 softPwm 초기화
+    if(!init_flag)
+    {
+        pinMode(BUZZER_PIN, OUTPUT);
+        digitalWrite(BUZZER_PIN, LOW); // 초기 값 off
+        init_flag = 1;
+    }
+
+    // state가 1이면 키고, 0이면 끄기
+    digitalWrite(BUZZER_PIN, state ? HIGH : LOW)
+}
---
+++ src/led.c
@@ -0,0 +1,28 @@
+#include <wiringPi.h>
+#include <softPwm.h>
+
+#include "hardware.h"
+
+#define LED_PIN 1 // WiringPi 1번 (BCM 18)
+
+void set_led_brightness(int duty_cycle) {
+    static int init_flag = 0;
+
+    // init_flag를 사용해 최초 한 번만 softPwm 초기화
+    if(!init_flag)
+    {
+        pinMode(LED_PIN, OUTPUT);
+        softPwmCreate(LED_PIN, 0, 255);
+        init_flag = 1;
+    }
+
+    // 예외 처리
+    if (duty_cycle < 0) {
+        duty_cycle = 0;
+    }
+    
+    if (duty_cycle > 255){
+        duty_cycle = 255;
+    }
+        
+}
